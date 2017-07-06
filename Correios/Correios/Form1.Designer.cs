4eb4a6adc78b1a2cf24dbc817734d46ba540b717
Raphael Soares <raphael.ss94@hotmail.com>
2017-07-05 23:46:11 -0300

    Add files via upload

---
+++ Correios/Correios/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Correios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCep = new System.Windows.Forms.TextBox();
+            this.lblResposta = new System.Windows.Forms.Label();
+            this.btnConsulta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CEP";
+            // 
+            // txtCep
+            // 
+            this.txtCep.Location = new System.Drawing.Point(47, 32);
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(239, 20);
+            this.txtCep.TabIndex = 1;
+            // 
+            // lblResposta
+            // 
+            this.lblResposta.AutoSize = true;
+            this.lblResposta.Location = new System.Drawing.Point(44, 117);
+            this.lblResposta.Name = "lblResposta";
+            this.lblResposta.Size = new System.Drawing.Size(0, 13);
+            this.lblResposta.TabIndex = 2;
+            // 
+            // btnConsulta
+            // 
+            this.btnConsulta.Location = new System.Drawing.Point(141, 75);
+            this.btnConsulta.Name = "btnConsulta";
+            this.btnConsulta.Size = new System.Drawing.Size(75, 23);
+            this.btnConsulta.TabIndex = 3;
+            this.btnConsulta.Text = "Consultar";
+            this.btnConsulta.UseVisualStyleBackColor = true;
+            this.btnConsulta.Click += new System.EventHandler(this.btnConsulta_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 261);
+            this.Controls.Add(this.btnConsulta);
+            this.Controls.Add(this.lblResposta);
+            this.Controls.Add(this.txtCep);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCep;
+        private System.Windows.Forms.Label lblResposta;
+        private System.Windows.Forms.Button btnConsulta;
+    }
+}
+
---
+++ Correios/Correios/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Correios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConsulta_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var correio = new Correio.AtendeClienteClient();
+                var resposta = correio.consultaCEP(txtCep.Text);
+                lblResposta.Text = "Endereço: " + resposta.end;
+                lblResposta.Text += "\nCidade: " + resposta.cidade;
+                lblResposta.Text += "\nBairro: " + resposta.bairro;
+                lblResposta.Text += "\nEstado: " + resposta.uf;
+            }
+            catch(Exception ex)
+            {
+                lblResposta.Text = "Erro ao efetuar consulta\n";
+                 
+            }
+        }
+    }
+}
